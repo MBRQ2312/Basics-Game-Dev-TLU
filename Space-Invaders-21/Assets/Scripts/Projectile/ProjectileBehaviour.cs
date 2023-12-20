869918545577029881c2c24988bd637cd5eb0f7e
MBRQ2312 <145580033+MBRQ2312@users.noreply.github.com>
2023-12-20 14:56:13 +0200

    I tried my best
    At least I did some changes. No end screen, though :(

--- Space-Invaders-21/Assets/Scripts/Projectile/ProjectileBehaviour.cs
+++ Space-Invaders-21/Assets/Scripts/Projectile/ProjectileBehaviour.cs
@@ -11,6 +11,7 @@ public class ProjectileBehaviour : MonoBehaviour
 
     // Start is called before the first frame update
     void Start()
+   
     {
 		// Another form of the Destroy function, which allows us to destroy an object
 		// after a delay in seconds. We set the delay with a variable "destroyAfter"
@@ -25,4 +26,5 @@ public class ProjectileBehaviour : MonoBehaviour
 		// in different situations. (can do the same for X and then it'll move in any direction you want)
         transform.Translate(new Vector3(0f, verticalValue) * Time.deltaTime * moveSpeed);
     }
+    
 }
